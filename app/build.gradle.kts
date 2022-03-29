e4d32ffab2039fb2c5148757dd9a79fce98095eb
Mohsen Mirhoseini <msn.mirhoseini@gmail.com>
2022-03-29 12:02:18 +0200

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+val kotlinVersion = "1.6.10"
+val composeVersion = "1.1.1"
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.mohsenoid.myapplication"
+        minSdk = 23
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        // allWarningsAsErrors = true
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = composeVersion
+    }
+
+    packagingOptions {
+        resources {
+            excludes += "/META-INFinstabug_comment_hint_bug/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/*"
+        }
+    }
+
+    // lint {
+    //     ignoreWarnings = false
+    //     warningsAsErrors = true
+    // }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+    implementation("androidx.activity:activity-compose:1.4.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    // compose
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+}
