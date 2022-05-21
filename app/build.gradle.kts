ac563e73826fe7244e2c3c00ed32ecca0c365b01
Mohsen <msn.mirhoseini@gmail.com>
2022-05-21 14:57:41 +0200

    Update material

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -137,7 +137,7 @@ ktlint {
 dependencies {
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
+    implementation("com.google.android.material:material:1.6.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
     implementation("androidx.activity:activity-compose:1.4.0")
 
